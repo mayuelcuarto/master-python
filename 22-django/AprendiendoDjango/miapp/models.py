47b27b18177e569b39c1f6b479ac1627c4ae2465
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-02-10 18:01:11 -0500

    Cambio en modelos para ajustar nombres y ordenamientos

--- 22-django/AprendiendoDjango/miapp/models.py
+++ 22-django/AprendiendoDjango/miapp/models.py
@@ -10,7 +10,16 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = "Artículo"
+        verbose_name_plural = "Artículos"
+        ordering = ['-created_at']
+
 class Category(models.Model):
     name = models.CharField(max_length=110)
     description = models.CharField(max_length=250)
     created_at = models.DateField()
+
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
