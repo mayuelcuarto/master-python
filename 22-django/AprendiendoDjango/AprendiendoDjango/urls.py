6c1c37cace6ce359478baac914be2ce79343ac31
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-02-12 18:46:04 -0500

    Configuracion de directorio de imagenes para el admin django

--- 22-django/AprendiendoDjango/AprendiendoDjango/urls.py
+++ 22-django/AprendiendoDjango/AprendiendoDjango/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 # Importar app con mis vistas
 from miapp import views
@@ -38,3 +39,8 @@ urlpatterns = [
     path('create-article/', views.create_article, name="create"),
     path('create-full-article/', views.create_full_article, name="create_full")
 ]
+
+# Configuracion para cargar imagenes
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
