c32da274c1d5013dff22f1751c4bc5ea7134cb5d
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-02-23 23:21:41 -0500

    Creacion de vistas y urls en la app pages

--- 22-django/ProyectoDjango/pages/views.py
+++ 22-django/ProyectoDjango/pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def page(request):
+    return render(request, "pages/page.html", {
+        "title": "Página individual",
+        "page": "Hola Mundo desde la app Pages"
+    })
