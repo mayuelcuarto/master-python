eb14860a5a94fab386993e139f959ed6f19bbb71
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-02-25 20:46:33 -0500

    Agregando filtro para listar solo paginas publicas

--- 22-django/ProyectoDjango/pages/context_processors.py
+++ 22-django/ProyectoDjango/pages/context_processors.py
@@ -1,7 +1,7 @@
 from pages.models import Page
 
 def get_pages(request):
-    pages = Page.objects.values_list('id', 'title', 'slug')
+    pages = Page.objects.filter(public=True).values_list('id', 'title', 'slug')
 
     return {
         'pages': pages
