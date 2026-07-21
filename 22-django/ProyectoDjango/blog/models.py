03590b5fced5589a42d47b5ba1d5b3925256221a
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-07-20 19:32:47 -0500

    Armando vista de articulos y probando funcionalidad de relaciones de orm

--- 22-django/ProyectoDjango/blog/models.py
+++ 22-django/ProyectoDjango/blog/models.py
@@ -21,7 +21,7 @@ class Article(models.Model):
     image = models.ImageField(default='null', verbose_name='Imagen')
     public = models.BooleanField(verbose_name='¿Publicado?')
     user = models.ForeignKey(User, editable=False, verbose_name='Usuario', on_delete=models.CASCADE)
-    category = models.ManyToManyField(Category, verbose_name='Categorías', null=True, blank=True)
+    categories = models.ManyToManyField(Category, verbose_name='Categorías', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creado el')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Editado el')
 
