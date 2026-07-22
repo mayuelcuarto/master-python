3da6fe2d0a727701cfc46f0cb2ac7e79d8d7177d
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-07-21 20:37:27 -0500

    Creacion de context processor para categorias

--- 22-django/ProyectoDjango/ProyectoDjango/settings.py
+++ 22-django/ProyectoDjango/ProyectoDjango/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'pages.context_processors.get_pages'
+                'pages.context_processors.get_pages',
+                'blog.processor.get_categories'
             ],
         },
     },
