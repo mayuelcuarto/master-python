6de5b4cb3468ba7ff5bf60858037fa698e8832af
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-08-14 19:59:20 -0500

    Implementacion de funcionalidad de formulario de login

--- 22-django/ProyectoDjango/mainapp/views.py
+++ 22-django/ProyectoDjango/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 from mainapp.forms import RegisterForm
 
 # Create your views here.
@@ -33,6 +34,17 @@ def register_page(request):
     })
 
 def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            messages.warning(request, 'No te has identificado correctamente')
 
     return render(request, 'users/login.html', {
         'title': 'Identifícate'
