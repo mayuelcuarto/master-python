88b982715e9781f642f3e137986a9578bfea5f26
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-08-29 23:08:52 -0500

    Uso de decoradores y validaciones de login

--- 22-django/ProyectoDjango/mainapp/views.py
+++ 22-django/ProyectoDjango/mainapp/views.py
@@ -17,35 +17,45 @@ def about(request):
     })
 
 def register_page(request):
-    register_form = RegisterForm()
+    if request.user.is_authenticated:
+        return redirect('inicio')
+    else:
+        register_form = RegisterForm()
 
-    if request.method == 'POST':
-        register_form = RegisterForm(request.POST)
+        if request.method == 'POST':
+            register_form = RegisterForm(request.POST)
 
-        if register_form.is_valid():
-            register_form.save()
-            messages.success(request, 'Te has registrado correctamente')
+            if register_form.is_valid():
+                register_form.save()
+                messages.success(request, 'Te has registrado correctamente')
 
-            return redirect('inicio')
+                return redirect('inicio')
 
-    return render(request, 'users/register.html', {
-        'title': 'Registro',
-        'register_form': register_form
-    })
+        return render(request, 'users/register.html', {
+            'title': 'Registro',
+            'register_form': register_form
+        })
 
 def login_page(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('inicio')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('inicio')
-        else:
-            messages.warning(request, 'No te has identificado correctamente')
+            if user is not None:
+                login(request, user)
+                return redirect('inicio')
+            else:
+                messages.warning(request, 'No te has identificado correctamente')
 
-    return render(request, 'users/login.html', {
-        'title': 'Identifícate'
-    })
+        return render(request, 'users/login.html', {
+            'title': 'Identifícate'
+        })
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
