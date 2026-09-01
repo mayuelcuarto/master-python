0c5cc7207b90cf0a29123835941001002c453c72
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-08-31 20:29:37 -0500

    Creacion de rutas con flask

--- 23-flask/main.py
+++ 23-flask/main.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Aprendiendo Flask con Victor Robles'
+    return "Aprendiendo Flask con Victor Robles"
+
+@app.route('/informacion')
+def informacion():
+    return "<h1>Página de información</h1>"
+
+@app.route('/contacto')
+def contacto():
+    return "<h1>Página de contactos</h1>"
+
+@app.route('/lenguajes-de-programacion')
+def lenguajes():
+    return "<h1>Página de lenguajes</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
