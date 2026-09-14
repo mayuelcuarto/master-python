81d20b5532f0a75fb5525bfc64f411f4a1702de2
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-09-14 09:23:06 -0500

    Implementacion de render_template

--- 23-flask/main.py
+++ 23-flask/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Aprendiendo Flask con Victor Robles"
+    return render_template('index.html')
 
 @app.route('/informacion')
 @app.route('/informacion/<string:nombre>')
@@ -12,13 +12,9 @@ def index():
 def informacion(nombre = None, apellidos = None):
     texto = ""
     if nombre != None and apellidos != None:
-        texto = f"<h3>Bienvenido, {nombre} {apellidos}</h3>"
+        texto = f"Bienvenido, {nombre} {apellidos}"
 
-    return f"""
-            <h1>Página de información</h1>
-            <p>Esta es la página de información<p>
-            {texto}
-    """
+    return render_template('informacion.html', texto=texto)
 
 @app.route('/contacto')
 @app.route('/contacto/<redireccion>')
