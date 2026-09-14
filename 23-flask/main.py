2cb323e57a07badf9c5eebfa0973ab04803786e4
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-09-14 09:08:53 -0500

    Probando redirects en rutas de flask

--- 23-flask/main.py
+++ 23-flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -21,7 +21,12 @@ def informacion(nombre = None, apellidos = None):
     """
 
 @app.route('/contacto')
-def contacto():
+@app.route('/contacto/<redireccion>')
+def contacto(redireccion = None):
+
+    if redireccion is not None:
+        return redirect(url_for('lenguajes'))
+
     return "<h1>Página de contactos</h1>"
 
 @app.route('/lenguajes-de-programacion')
